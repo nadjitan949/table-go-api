323fd65d59f302b9547d9bf2aeff70275ea0645d
Nadjitan Betan <nadjitanb@gmail.com>
2026-08-30 10:58:17 +0000

    Environement initialized !

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log("Salut, bienvenue sur TableGo !");
+const express = require("express")
+const app = express()
+require("dotenv").config()
+const port = process.env.PORT || 5000
+
+app.get("/", (req, res) => {
+    try {
+
+        res.json({message: "Salut"})
+        
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+app.listen(port, () => {
+    try {
+        console.log(`Serveur en ligne sur http://localhost:${port}`)
+    } catch (error) {
+        console.log('Erreur survenus')
+    }
+})
