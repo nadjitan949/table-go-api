22c56c89c4f9a19b0db908c230bc7d8a0ea98814
Nadjitan Betan <nadjitanb@gmail.com>
2026-08-30 11:29:24 +0000

    chore: setup development environment

--- index.js
+++ index.js
@@ -1,22 +1,25 @@
-const express = require("express")
-const app = express()
-require("dotenv").config()
-const port = process.env.PORT || 5000
+const express = require('express');
+const app = express();
+require('dotenv').config();
+const sequelize = require('./database/config/connect');
+const port = process.env.PORT || 5000;
 
-app.get("/", (req, res) => {
-    try {
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-        res.json({message: "Salut"})
-        
-    } catch (error) {
-        console.log(error)
-    }
-})
+async function connection() {
+  try {
+    await sequelize.authenticate();
+    console.log('Connexion réussie');
+
+    await sequelize.sync({ alter: true });
+    console.log('Base de donnée synchronisé');
+  } catch (error) {
+    console.log(`Une erreur s'est produite: ${error}`);
+  }
+}
+connection();
 
 app.listen(port, () => {
-    try {
-        console.log(`Serveur en ligne sur http://localhost:${port}`)
-    } catch (error) {
-        console.log('Erreur survenus')
-    }
-})
+  console.log(`Serveur en ligne sur http://localhost:${port}`);
+});
