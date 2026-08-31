c22ff21e4b160e7a78b215045df4b65cfac3d4a4
Nadjitan Betan <nadjitanb@gmail.com>
2026-08-31 16:00:05 +0000

    feat(model): add menu items tables

--- database/config/index.js
+++ database/config/index.js
@@ -1,5 +1,6 @@
 const sequelize = require('./connect');
 const User = require('../model/tables/user.model');
 const Otp = require('../model/tables/otp.model');
+const MenuItem = require('../model/tables/menu.model');
 
-module.exports = { sequelize, User, Otp };
+module.exports = { sequelize, User, Otp, MenuItem };
