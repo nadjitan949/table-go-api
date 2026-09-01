10a26be1618af81ba09a60d862e4006d397da2c4
Nadjitan Betan <nadjitanb@gmail.com>
2026-09-01 17:07:34 +0000

    chore(model): add 'table' table

---
+++ database/model/tables/table.model.js
@@ -0,0 +1,17 @@
+const { DataTypes } = require("sequelize");
+const sequelize = require("../../config/connect");
+
+const Table = sequelize.define("Table",
+    {
+        id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true, unique: true },
+        number: { type: DataTypes.STRING, allowNull: false, unique: true },
+        qrCodeToken: { type: DataTypes.STRING, allowNull: false, unique: true },
+        status: { type: DataTypes.ENUM("free", "occupied", "out_of_service"), allowNull: false, defaultValue: "free" }
+    },
+    {
+        tableName: "tables",
+        timestamps: true,
+    }
+)
+
+module.exports = Table
