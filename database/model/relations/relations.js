6dfe5443f39eed8ee8cac5bc8705da17adab9f9d
Nadjitan Betan <nadjitanb@gmail.com>
2026-09-04 11:59:06 +0000

    feat(model): add relation menu-add_on

---
+++ database/model/relations/relations.js
@@ -0,0 +1,7 @@
+const AddOn = require("../tables/addOn.model");
+const MenuItem = require("../tables/menu.model");
+
+MenuItem.hasMany(AddOn, { foreignKey: "menuId", onDelete: "CASCADE" })
+AddOn.belongsTo(MenuItem, { foreignKey: "menuId", onDelete: "CASCADE" })
+
+module.exports = { MenuItem, AddOn }
--- database/model/tables/addOn.model.js
+++ database/model/tables/addOn.model.js
@@ -5,8 +5,10 @@ const AddOn = sequelize.define("AddOn",
     {
         id: { type: DataTypes.INTEGER, primaryKey: true, unique: true, autoIncrement: true },
         name: { type: DataTypes.STRING, allowNull: false },
+        price: { type: DataTypes.DECIMAL(10, 2) },
         description: { type: DataTypes.TEXT, allowNull: true },
-        image: { type: DataTypes.STRING, allowNull: true }
+        image: { type: DataTypes.STRING, allowNull: true },
+        menuId: { type: DataTypes.INTEGER, allowNull: false }
     },
     {
         tableName: "add_on",
